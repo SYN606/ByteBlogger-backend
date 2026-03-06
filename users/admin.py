8ff9dc315b4acc3cae7b1d41fc28b03076984fc1
syn <postbox.501@yandex.com>
2026-03-06 06:15:30 +0000

    fixed known bugs in admin.py of users app

--- users/admin.py
+++ users/admin.py
@@ -2,51 +2,105 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, UserProfile, OTPRequest
 
-# @admin.register(User)
-# class UserAdmin(BaseUserAdmin):
-#     model = User
 
-#     list_display = ('id', 'email', 'username', 'is_verified', 'is_staff',
-#                     'is_superuser')
+class UserAdmin(BaseUserAdmin):
+    model = User
 
-#     list_filter = ('is_verified', 'is_staff', 'is_superuser')
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "is_verified",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
 
-#     search_fields = ('email', 'username')
+    list_filter = (
+        "is_verified",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
 
-#     ordering = ('email', )
+    search_fields = ("email", "username")
+    ordering = ("email",)
 
-#     fieldsets = BaseUserAdmin.fieldsets + (('Custom Fields', {
-#         'fields': ('is_verified', ),
-#     }), )  # type: ignore
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Custom Fields", {
+            "fields": ("is_verified",),
+        }),
+    )
 
-#     add_fieldsets = BaseUserAdmin.add_fieldsets + (('Custom Fields', {
-#         'fields': ('is_verified', ),
-#     }), )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ("Custom Fields", {
+            "fields": ("is_verified",),
+        }),
+    )
 
-admin.site.register(User)
+
+admin.site.register(User, UserAdmin)
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'full_name', 'topic_interests')
-    search_fields = ('user__email', 'full_name', 'topic_interests')
+    list_display = (
+        "id",
+        "user",
+        "full_name",
+        "topic_interests",
+    )
+
+    search_fields = (
+        "user__email",
+        "full_name",
+        "topic_interests",
+    )
 
 
 @admin.register(OTPRequest)
 class OTPRequestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'get_email', 'otp', 'request_time',
-                    'expiration_time', 'is_expired')
-    list_filter = ('request_time', 'expiration_time', 'user')
-    search_fields = ('user__email', 'otp')
-    ordering = ('-request_time', )
+
+    list_display = (
+        "id",
+        "user",
+        "get_email",
+        "masked_otp",
+        "created_at",
+        "expiration_time",
+        "is_expired",
+    )
+
+    list_filter = (
+        "created_at",
+        "expiration_time",
+        "user",
+    )
+
+    search_fields = (
+        "user__email",
+        "email",
+        "otp",
+    )
+
+    ordering = ("-created_at",)
+
+    readonly_fields = (
+        "created_at",
+        "expiration_time",
+    )
 
     def get_email(self, obj):
         return obj.user.email
+    get_email.short_description = "Email"
 
-    get_email.short_description = 'Email'
+    def masked_otp(self, obj):
+        if obj.otp:
+            return obj.otp[:6] + "****"
+        return "-"
+    masked_otp.short_description = "OTP"
 
     def is_expired(self, obj):
         return obj.is_expired()
-
     is_expired.boolean = True
-    is_expired.short_description = 'Expired'
+    is_expired.short_description = "Expired"
